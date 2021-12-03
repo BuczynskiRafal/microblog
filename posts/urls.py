14396de2f3d3c6b36a4ced45cae5aa03eaf43008
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-12-03 12:06:03 +0100

    posts details site

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('posts/', post_list, name='post_list')
+    path('posts/<int:post_id>', post_details, name='post_detail'),
+    path('posts/', post_list, name='post_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'list.html', {'posts':posts})
+
+
+def post_details(request, post_id: int):
+    detail= Post.objects.get(pk=post_id)
+    return render(request, 'post_details.html', {'detail':detail})
