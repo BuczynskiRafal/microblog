cb4100e1f535a6887558856988bb4355e490bea4
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-12-16 14:54:40 +0100

    UserProfile create

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    bio = models.TextField()
