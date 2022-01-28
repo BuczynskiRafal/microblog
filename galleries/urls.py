a3fc48129fd05e7ce331155c54c0c87238bb3008
BuczynskiRafal <buczynski.rafal2@gmail.com>
2022-01-28 14:24:52 +0100

    Fixed adding photo to gallery.

--- galleries/urls.py
+++ galleries/urls.py
@@ -6,6 +6,6 @@ app_name = 'galleries'
 urlpatterns = [
     path("add/", add_gallery_view, name="add_gallery"),
     path('<int:gallery_id>/', gallery_details, name="gallery_details"),
-    path('<int:gallery_id>/add', add_photo_view, name="add_photo"),
+    path('<int:gallery_id>/add/', add_photo_view, name="add_photo"),
     path('', galleries_list, name='galleries_list'),
 ]
